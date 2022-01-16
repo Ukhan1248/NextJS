ea2e649d7bd368a2d4c6514a594086f50f7f3449
Umair Khan <umair.khan.ece@gmail.com>
2022-01-16 17:46:09 -0600

    Commit 2 1/16/22

--- eventspages/components/events/event-item.js
+++ eventspages/components/events/event-item.js
@@ -1,5 +1,7 @@
-import Link from "next/link";
-
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 import classes from "./event-item.module.css";
 
 function EventItem(props) {
@@ -16,18 +18,25 @@ function EventItem(props) {
   return (
     <li className={classes.item}>
       <img src={"/" + image} alt={title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
+          <div className={classes.date}>
+            <DateIcon />
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <AddressIcon />
+            <address>{formattedAddress}</address>
+          </div>
         </div>
-        <div>
-          <time>{humanReadableDate}</time>
-        </div>
-        <div>
-          <address>{formattedAddress}</address>
-        </div>
-        <div>
-          <Link href="/">Explore Event</Link>
+        <div className={classes.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
--- eventspages/components/events/event-list.js
+++ eventspages/components/events/event-list.js
@@ -1,10 +1,11 @@
 import EventItem from "./event-item";
+import classes from "./event-list.module.css";
 
 function EventList(props) {
   const { items } = props;
 
   return (
-    <ul className="item">
+    <ul className={classes.list}>
       {items.map(event => (
         <EventItem
           key={event.id}
