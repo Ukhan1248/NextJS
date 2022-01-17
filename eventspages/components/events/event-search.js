05d43864287fd3575b4ea12ddf514bed1a0334b1
Umair Khan <umair.khan.ece@gmail.com>
2022-01-17 08:59:03 -0600

    Commit 1 1/17/22

---
+++ eventspages/components/events/event-search.js
@@ -0,0 +1,19 @@
+function EventsSearch(props) {
+  return (
+    <form>
+      <div>
+        <div>
+          <label htmlFor="year">Year</label>
+          <select id="year">
+            <option value="2021">2021</option>
+            <option value="2021">2021</option>
+          </select>
+          </div>
+        </div>
+        <label htmlFor>Month</label>
+      </div>
+    </form>
+  );
+}
+
+export default EventsSearch;
