11e5d4f001d9b37c68f663679a3463fa6241cd77
Umair Khan <umair.khan.ece@gmail.com>
2022-01-22 12:08:45 -0600

    Commit 1 1/22/22

---
+++ eventspages/helpers/api-util.js
@@ -0,0 +1,22 @@
+export async function getAllEvents() {
+  const response = await fetch(
+    "https://nextjs-course-66925-default-rtdb.firebaseio.com/events.json"
+  );
+  const data = await response.json();
+
+  const events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+  return allEvents.filter(event => event.isFeatured);
+}
