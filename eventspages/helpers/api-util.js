1fd16224ac2be55c9105c0fa08a7c01680ea0ad5
Umair Khan <umair.khan.ece@gmail.com>
2022-01-25 09:38:24 -0600

    Commit 2 1/25/22
    Fixed error with firebase link, and .jpg image

--- eventspages/helpers/api-util.js
+++ eventspages/helpers/api-util.js
@@ -1,6 +1,6 @@
 export async function getAllEvents() {
   const response = await fetch(
-    "https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json"
+    "https://nextjs-course-66925-default-rtdb.firebaseio.com/events.json"
   );
   const data = await response.json();
 
--- eventspages/pages/events/[...slug].js
+++ eventspages/pages/events/[...slug].js
@@ -15,7 +15,7 @@ function FilteredEventsPage(props) {
   const filterData = router.query.slug;
 
   const { data, error } = useSWR(
-    "https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json"
+    "https://nextjs-course-66925-default-rtdb.firebaseio.com/events.json"
   );
 
   useEffect(() => {
