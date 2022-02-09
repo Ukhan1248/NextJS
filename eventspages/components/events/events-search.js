cbe8482e8d8d4cda0ab686d59cbdb66f4879e772
Umair Khan <umair.khan.ece@gmail.com>
2022-02-08 20:07:00 -0600

    Commit 1 2/8/22

--- eventspages/components/events/events-search.js
+++ eventspages/components/events/events-search.js
@@ -25,6 +25,9 @@ function EventsSearch(props) {
             <option value="2021">2021</option>
             <option value="2022">2022</option>
             <option value="2023">2023</option>
+            <option value="2024">2024</option>
+            <option value="2025">2025</option>
+            <option value="2026">2026</option>
           </select>
         </div>
         <div className={classes.control}>
